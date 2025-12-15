0c0da0ff879c03ee47a39021611b35f8c72f531f
Nadav Masliah <nadavma2@mta.ac.il>
2025-12-15 17:42:58 +0200

    Code for EX2

---
+++ EX2/spark-word-count.py
@@ -0,0 +1,35 @@
+import sys
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: wordcount <input_folder>", file=sys.stderr)
+        sys.exit(-1)
+
+    conf = SparkConf().setAppName("python-word-count")
+    sc = SparkContext(conf=conf)
+
+    text_file = sc.textFile("hdfs://" + sys.argv[1])
+
+    all_words = (
+        text_file
+        .flatMap(lambda line: line.split(" "))
+        .map(lambda word: word.strip())
+        .filter(lambda word: word != "")
+        .cache())
+
+    counts = (
+        all_words
+        .map(lambda word: (word, 1))
+        .reduceByKey(lambda a, b: a + b)
+        .filter(lambda x: len(x[0]) > 5)
+        .repartition(5))
+
+    top40 = counts.takeOrdered(40, key=lambda x: -x[1])
+
+    distinct_words_count = all_words.distinct().count()
+
+    print("--------------------------------------------")
+    print(*top40, sep="\n")
+    print("--------------------------------------------")
+    print("Total number of distinct words:", distinct_words_count)
